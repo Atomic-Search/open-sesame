6f5f7fae409956987e9187f4b33a6b66ba9ead53
Alan Hogue <eahogue@gmail.com>
2021-03-04 11:51:47 -0800

    Trying different trainer.

--- sesame/frameid.py
+++ sesame/frameid.py
@@ -24,7 +24,7 @@ import time
 import tqdm
 from optparse import OptionParser
 
-from dynet import Model, LSTMBuilder, SimpleSGDTrainer, lookup, concatenate, rectify, renew_cg, dropout, log_softmax, esum, pick
+from dynet import Model, LSTMBuilder, SimpleSGDTrainer, lookup, concatenate, rectify, renew_cg, dropout, log_softmax, esum, pick, AdamTrainer
 
 from .conll09 import lock_dicts, post_train_lock_dicts, VOCDICT, POSDICT, FRAMEDICT, LUDICT, LUPOSDICT
 from .dataio import get_wvec_map, read_conll, read_related_lus
@@ -181,8 +181,8 @@ print_data_status(FRAMEDICT, "Frames")
 sys.stderr.write("\n_____________________\n\n")
 
 model = Model()
-trainer = SimpleSGDTrainer(model, 0.01)
-# trainer = AdamTrainer(model, 0.0001, 0.01, 0.9999, 1e-8)
+#trainer = SimpleSGDTrainer(model)
+trainer = AdamTrainer(model, 0.0001, 0.01, 0.9999, 1e-8)
 
 v_x = model.add_lookup_parameters((VOCDICT.size(), TOKDIM))
 p_x = model.add_lookup_parameters((POSDICT.size(), POSDIM))
